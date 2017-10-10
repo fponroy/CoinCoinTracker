c37f191e6038ff80382dd3b27acab0c5b2eeb8f7
Florian PONROY - OpenClassrooms <florian.ponroy@openclassrooms.com>
2017-10-10 16:07:59 +0200

    Moved the MainActivity class within a specific controller folder

--- app/src/main/java/com/ponroy/florian/coincointracker/controller/MainActivity.java
+++ app/src/main/java/com/ponroy/florian/coincointracker/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ponroy.florian.coincointracker;
+package com.ponroy.florian.coincointracker.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ponroy.florian.coincointracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
